5dcbd58e7344a1625b187f805f1e9f46f3caf13e
김재홍 <jjaekong@gmail.com>
2016-06-10 15:49:28 +0900

    초기화

---
+++ inc/header.php
@@ -0,0 +1,39 @@
+<a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+<a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
+<header id="header">
+    <div class="container">
+        <div class="logo">
+            <h1><a href="http://www.paekche.ac.kr" target="_blank"><img src="/assets/images/logo.png" alt="백제예술대학교"></a></h1>
+            <h2><a href="/assets/main.php"></a><a href="/">입학안내</a></h2>
+        </div>
+        <nav id="gnb" tabindex="-1">
+            <ul>
+                <li><a href="#">학과안내</a></li>
+                <li class="has-child">
+                    <a href="#">모집요강</a>
+                    <ul>
+                        <li><a href="#">전형일정</a></li>
+                        <li><a href="#">모집학과 및 모집인원</a></li>
+                        <li><a href="#">응시자격</a></li>
+                        <li><a href="#">전형방법 및 선발원칙</a></li>
+                        <li><a href="#">실기/면접시험 방법</a></li>
+                        <li><a href="#">제출서류 및 전형료</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">입학 Q&amp;A</a></li>
+                <li class="has-child">
+                    <a href="#">입시정보</a>
+                    <ul>
+                        <li><a href="#">공지사항</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">경쟁률</a></li>
+                        <li><a href="#">자료실</a></li>
+                        <li><a href="#">백제스타</a></li>
+                        <li><a href="#">내신성적산출</a></li>
+                        <li><a href="#">입시자료신청</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main id="content">
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>

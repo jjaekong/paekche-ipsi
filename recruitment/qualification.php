ad7b67755076dc38592b851466e3798fe7a68c1c
kyh5392 <kyh90495392@gmail.com>
2016-06-10 17:51:48 +0900

    [공통] sub.css 작성완료. #1

--- recruitment/qualification.php
+++ recruitment/qualification.php
@@ -1,5 +1,6 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
-<link rel="stylesheet" href="/assets/recruitment/recruitment.css" />
+<link rel="stylesheet" href="/assets/css/sub.css" />
+<link rel="stylesheet" href="/assets/css/recruitment.css" />
 </head>
 <body>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
@@ -27,9 +28,134 @@
 			</ol>
 		</div>
 	</div>
-	<main id="content">
-		
+	<main id="content" class="recruitment qualification">
+		<div class="container">
+			<section class="sub">
+				<div class="section-header">
+					<hr />
+					<h3>일반 전형</h3>
+				</div>
+				<div class="section-content">
+					<ul class="content-list">
+						<li>고등학교 졸업자 또는 2016년 2월 고등학교 졸업예정자</li>
+						<li>고등교육법에 의해 위와 동등 이상의 학력이 있다고 인정된 자 (고등교육법 제33조, 초중등교육법 시행령 제98조) </li>
+						<li>고등학교 졸업학력 검정고시 합격자</li>
+					</ul>
+				</div>
+				<div class="section-header">
+					<hr />
+					<h3>특별 전형</h3>
+				</div>
+				<div class="section-content">
+					<div class="table-wrap">
+						<table class="table table-border">
+							<colgroup>
+								<col style="width: 100px"/>
+								<col style="width: 100px"/>
+								<col style="width: 100px"/>
+								<col style="width: 100px"/>
+							</colgroup>
+							<thead> 
+								<tr>
+									<th>제목</th>
+									<th>제목</th>
+									<th>제목</th>
+									<th>제목</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>내용</td>
+									<td>내용</td>
+									<td>내용</td>
+									<td>내용</td>
+								</tr>
+								<tr>
+									<td>내용</td>
+									<td>내용</td>
+									<td>내용</td>
+									<td>내용</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+
+
+
+
+				<ul class="content-list" style="margin:30px 0;">
+					<li>고등학교 졸업자 또는 2016년 2월 고등학교 졸업예정자</li>
+					<li>고등학교 졸업자 또는 2016년 2월 고등학교 졸업예정자</li>
+					<li>고등학교 졸업자 또는 2016년 2월 고등학교 졸업예정자</li>
+				</ul>
+				<ol class="circle-list" style="margin:30px 0;">
+					<li>지방자치법 제3조에 의한 읍면(도서·벽지)에 소재한 고등학교의 전 교육과정을 이수하고,</li>
+					<li>고등학교 재학기간 동안 본인과 부모 모두가 농어촌 지역에 거주한 자로서 출신 고등학교장의 확인을 받은 자.</li>
+				</ol>
+
+				<div class="btn-area">
+					<p>
+						<a class="btn btn-sm btn-gray" href="#" role="button">목록</a>
+						<a class="btn btn-sm btn-white" href="#" role="button">수정</a>
+						<a class="btn btn-sm btn-white" href="#" role="button">삭제</a>
+						<a class="btn btn-sm btn-pink" href="#" role="button">글쓰기</a>
+					</p>
+				</div>
+
+				<div class="btn-area">
+					<p>
+						<button type="submit" class="btn btn-pink">확인</button>
+						<a class="btn btn-gray" href="#" role="button">취소</a>
+					</p>
+				</div>
+
+				<nav class="paging">
+					<ul class="pagination">
+						<li>
+							<a href="#" aria-label="first-Previous">
+								<span aria-hidden="true">&laquo;</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" aria-label="Previous">
+								<span aria-hidden="true">&lsaquo;</span>
+							</a>
+						</li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li>
+							<a href="#" aria-label="Next" >
+								<span aria-hidden="true">&rsaquo;</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" aria-label="end-Next">
+								<span aria-hidden="true">&raquo;</span>
+							</a>
+						</li>
+					</ul>
+				</nav>
+
+				<div class="search-area" style="margin:30px 0;">
+					<form>
+						<div class="form-group">
+							<label for="search-keyword" class="sr-only">검색어</label>
+							<input type="text" name="#" id="search-keyword" class="form-control" placeholder="Search">
+						</div>
+						<button type="submit" class="btn"><span class="glyphicon glyphicon-search"></span><span class="sr-only">검색</span></button>
+					</form>
+				</div>
+
+			</section>
+		</div>
 	</main>
+<style>
+
+</style>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
 	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
 </body>

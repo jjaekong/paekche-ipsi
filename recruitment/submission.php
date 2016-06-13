b0eb7f9af85ba9bf3e8ad81e97eda827c3864ab3
kyh5392 <kyh90495392@gmail.com>
2016-06-13 19:52:05 +0900

    [모집요강] > 제출서류 및 전형료 #1 (작업중)

---
+++ recruitment/submission.php
@@ -0,0 +1,416 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+<link rel="stylesheet" href="/assets/css/recruitment.css" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="jumbo-wall">
+		<div class="container">
+			<p>
+				<small>우리문화예술의 정통성을 바탕으로 그 본류를 주도하고</small><br>
+				미래를 선도하는 예술인 교육
+			</p>
+		</div>
+	</div>
+	<div class="page-nav">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">모집요강</a></li>
+				<!-- li class="active">수업자료실</li -->
+				<li class="active dropdown">
+					<a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">제출서류 및 전형료<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">제출서류 및 전형료</a></li>
+					</ul>
+				</li>
+			</ol>
+		</div>
+	</div>
+	<main id="content" class="recruitment submission">
+		<div class="container">
+			<section class="submission-document">
+				<div class="section-header">
+					<hr />
+					<h3>제출서류 및 전형료</h3>
+				</div>
+				<div class="section-content">
+					<div class="table-wrap">
+						<table class="table table-border">
+							<colgroup>
+								<col style="width: 90px"/>
+								<col style="width: 90px"/>
+								<col style="width: 795px"/>
+								<col style="width: 195px"/>
+							</colgroup>
+							<thead> 
+								<tr>
+									<th colspan="2">전형구분</th>
+									<th>제출서류</th>
+									<th>비고</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td colspan="2">입학원서</td>
+									<td>인터넷 원서접수자는 대학에서 일괄 출력하므로 제출하지 않습니다.</td>
+									<td></td>
+								</tr>
+								<tr>
+									<td colspan="2">학교생활기록부</td>
+									<td class="table-padding-two">
+										<ol class="circle-list">
+											<li>2011년 2월 이전 졸업자</li>
+											<li>온라인제공 미동의자</li>
+											<li>온라인제공이 되지 않는 고등학교</li>
+										</ol>
+									</td>
+									<td>학교생활기록부 미제출 시<br> 불합격 처리함</td>
+								</tr>
+								<tr>
+									<td colspan="2">검정고시 출신자</td>
+									<td class="table-padding-one">
+										<ol class="circle-list">
+											<li>합격증명서 1부</li>
+											<li>성적증명서 1부</li>
+										</ol>
+									</td>
+									<td>고등학교졸업학력검정고시<br> 합격자</td>
+								</tr>
+								<tr>
+									<td colspan="2">특별전형</td>
+									<td>해당 증빙서류</td>
+									<td>특별전형 자격기준 참조</td>
+								</tr>
+								<tr>
+									<td colspan="2">국가보훈대상자</td>
+									<td>대학입학특별전형대상자 증명서</td>
+									<td>보훈처</td>
+								</tr>
+								<tr>
+									<td colspan="2">전문대졸 이상자</td>
+									<td  class="table-padding-one">
+										<ol class="circle-list">
+											<li>대학 학위(예정)증명서 1부</li>
+											<li>대학 전학년 성적증명서 1부</li>
+										</ol>
+									</td>
+									<td>출신대학</td>
+								</tr>
+								<tr>
+									<td colspan="2">농어촌 출신자</td>
+									<td>
+										<ol class="circle-list">
+											<li>
+												 고등학교장 추천서 1부(우리대학 소정양식)
+												 <ul class="arrow-list">
+													<li>입학안내 홈페이지</li>
+													<li>입시정보</li>
+													<li>자료실</li>
+												 </ul>
+											</li>
+											<li>주민등록초본 : 학생 본인, 보호자(부/모) 각 1부<br>가족관계증명서 (학생 본인 명의) 1부</li>
+										</ol>
+									</td>
+									<td>추가 서류 요청시 제출 요망</td>
+								</tr>
+								<tr>
+									<td colspan="2">기초생활수급자</td>
+									<td>
+										<ul>
+											<li>국민기초생활수급자증명서 (지원자 명의)</li>
+											<li>가족관계증명서 (학생 본인 명의) 1부</li>
+										</ul>
+									</td>
+									<td>시·군·구청/ <br>읍·면·동 주민센터</td>
+								</tr>
+								<tr>
+									<td rowspan="2">차상위<br>계층</td>
+									<td rowspan="2" class="table-align">복지급여<br>수급자</td>
+									<td class="table-padding-three">
+										<ol class="circle-list">
+											<li>
+												 차상위 복지급여 수급확인서
+												 <ul class="standard-list">
+													<li>
+														<dl class="dl-inline">
+															<dt>장애수당/장애인연금 차상위부가급여</dt>
+															<dd>장애연금·장애연금수급자증명서</dd>
+														</dl>
+													</li>
+													<li>
+														<dl class="dl-inline">
+															<dt>자활급여대상자</dt>
+															<dd>자활근로자확인서</dd>
+														</dl>
+													</li>
+													<li>
+														<dl class="dl-inline">
+															<dt>한부모가정지원사업 대상자</dt>
+															<dd>한부모가족증명서</dd>
+														</dl>
+													</li>
+													<li>
+														가족관계증명서 (학생 본인 명의) 1부
+													</li>
+												 </ul>
+											</li>
+										</ol>
+									</td>
+									<td>시·군·구청/ <br>읍·면·동 주민센터</td>
+								</tr>
+								<tr>
+									<td class="table-align table-padding-three">
+										<ol class="circle-list">
+											<li class="sr-only"></li>
+											<li class="after-list">
+												 차상위본인부담경감 대상자 확인서
+												 <ul class="standard-list">
+													<li>차상위본인부담경감 대상자 증명서</li>
+													<li>가족관계증명서 (학생 본인명의) 1부</li>
+												 </ul>
+											</li>
+										</ol>
+									</td>
+									<td>국민건강보험공단</td>
+								</tr>
+								<tr>
+									<td colspan="2">복지급여<br>수급자</td>
+									<td>
+										<ol class="circle-list">
+											<li>입학원서(소정양식)</li>
+											<li>고등학교 졸업증(졸업예정증명서) 1부</li>
+											<li>고등학교 성적증명서 1부 (고교 졸업 및 성적증명서 원본과 한글번역 공증본 제출)</li>
+											<li>본인여권사본</li>
+											<li>
+												자격요건 해당자 : 가족관계 입증서류
+												<p class="attention">가족관계 입증서류(원본 및 국문 또는 영문 번역본 첨부)</p>
+											</li>
+											<li>
+												중국인 학생은 학위인증센터(<a href="www.cdgdc.edu.cn" target="_blank">www.cdgdc.edu.cn</a>)에서 성적표 포함 발급된 고등학교졸업고사 증서 원본 및 영문인증서 원본제출
+												<p class="attention">인증서를 제출할 수 없는 경우 지원 불가</p>
+												<p class="attention">외국인 유학생의 학력, 학위입증서류 안내 참조</p>
+											</li>
+											<li>
+												중국 제외한 기타 국가 : 졸업(예정)증명서 및 성적증명서에 대한 "아포스티유 확인서" 를 받거나<br>
+												또는 주재국 한국영사관/ 주한 자국공관에서 "영사 확인"을 받은 후 제출
+												<p class="attention">
+													서류접수 시 학력입증 확인 발급이 미완료되었을 경우, 졸업증명서 사본 및 아포스티유 또는 영사확인서 신청 접수증을 제출한 뒤,<br>졸업.증명서 원본 및 아포스티유 또는 영사확인서를 제출하여야함.
+												</p>
+											</li>
+										</ol>
+									</td>
+									<td>
+										<h4>가족관계 입증서류 예시</h4>
+										<dl class="dl-inline">
+											<dt>중국</dt>
+											<dd>호구부 또는<br> 친족관계공증</dd>
+											<br>
+											<dt>몽골</dt>
+											<dd>친족관계증명서</dd>
+											<br>
+											<dt>필리핀</dt>
+											<dd>Family Census</dd>
+											<br>
+											<dt>태국</dt>
+											<dd>출생증명서 등</dd>
+										</dl>
+										<h4>중국거주자 추가 제출서류</h4>
+										<ol class="number-list">
+											<li>
+												호구부 원본 및 번역본
+												<p class="standard">전가족 사항이 기재 되어 있어야 함.</p>
+											</li>
+											<li>
+												부모의 재직증명서
+												<p class="standard">재직기관, 재직기간, 직위, 수입내역(월급여액 또는 연급여액), 발급부서 연락처, 중국어로 된 증명서 한글 번역문 첨부</p>
+											</li>
+										</ol>
+										<h4>중국체류자 은행잔고증명서 제출</h4>
+										<ol class="number-list">
+											<li>재정보증인이 미화 12,000달러 이상을 예치했다는 증명</li>
+											<li>증명서 발급일 및 동결일 기재</li>
+										</ol>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<p class="attention">제출서류는 모집시기별 원서접수 마감시간까지 제출해야 하며, 제출한 서류는 일체 반환하지 않습니다. [마감일 우체국 소인까지 유효함] </p>
+						<p class="attention"><b>제출서류 발급일자는 원서접수 마감일 기준 1개월 이내</b>여야 하며, 추후 위·변조 사실이 발견될 경우 입학이 취소됩니다. </p>
+						<p class="attention">개명을 한 수험생은 <b>'주민등록초본'</b>을 제출해야 합니다. </p>
+						<p class="attention">서류 미제출자는 불합격 처리합니다.</p>
+					</div>
+				</div>
+			</section>
+			<section class="application-fee">
+				<div class="section-header">
+					<hr />
+					<h3>전형료</h3>
+				</div>
+				<div class="section-content">
+					<div class="table-wrap">
+						<table class="table table-border">
+							<colgroup>
+								<col style="width: 90px"/>
+								<col style="width: 90px"/>
+								<col style="width: 795px"/>
+								<col style="width: 195px"/>
+							</colgroup>
+							<tbody>
+								<tr>
+									<th colspan="2">실기 미실시학과</th>
+									<td>30,000원</td>
+									<td rowspan="2">수수료<br>5,000원 별도</td>
+								</tr>
+								<tr>
+									<th colspan="2">실기 실시학과</th>
+									<td>50,000원 [전형료 30,000원 + 응시료 20,000원]</td>
+								</tr>
+								<tr>
+									<th colspan="2">전형료 면제</th>
+									<td>기초생활수급자(차상위) <p class="attention" style="display:inline;">단, 원서접수 대행업체의 수수료(5,000원)는 부담해야 함.</p></td>
+									<td></td>
+								</tr>
+								<tr>
+									<th colspan="2">전형료 면제</th>
+									<td colspan="2">고등교육법시행령 제42조의3(입학전형료)</td>
+								</tr>
+								<tr>
+									<th rowspan="3">전형료<br>반환</th>
+									<th>사유</th>
+									<td colspan="2">
+										<ol class="circle-list">
+											<li>입학전형에 응시한 사람이 착오로 과납한 경우 : 과납한 금액</li>
+											<li>대학의 귀책사유로 입학전형에 응시하지 못한 경우 : 납부한 입학전형료 전액</li>
+											<li>천재지변으로 인하여 입학전형에 응시하지 못한 경우 : 납부한 입학전형료 전액</li>
+											<li>
+												질병 또는 사고 등으로 의료기관에 입원하거나 본인의 사망으로 입학전형에 응시하지 못한 경우<br>
+												<dl class="dl-inline">
+													<dt>(해당 사항을 증명할 수 있는 경우에만 해당한다)</dt>
+													<dd>납부한 입학전형료 전액</dd> 
+												</dl>
+											</li>
+										</ol>
+									</td>
+								</tr>
+								<tr>
+									<th>방법</th>
+									<td colspan="2">
+										<ol class="circle-list">
+											<li>
+												입학전형 관련 수입·지출에 따른 잔액을 해당 학년도 4월 30일까지 입학전형에 응시한 사람이 납부한<br>
+												입학전형료에 비례하여 반환하여야 한다.
+											</li>
+											<li>
+												입학전형료를 반환하는 경우 둘 이상의 반환 방법을 마련하여 반환 대상자가 선택할 수 있도록 하여야 한다.<br>
+												이 경우 그 반환 방법에는 학교를 직접 방문하는 방법과 반환 대상자가 지정하는 금융기관의 계좌로<br>
+												이체하는 방법이 반드시 포함되어야 한다.
+											</li>
+											<li>
+												반환 대상자가 금융기관의 계좌로 이체하는 방법을 선택하는 경우에는 반환할 금액에서 금융기관의 전산망을 이용하는 데<br>
+												드는 비용을 차감하고 반환할 수 있다.
+											</li>
+											<li>금융기관의 전산망을 이용하는 데 드는 비용이 반환할 금액 이상이면 반환하지 아니할 수 있다.</li>
+										</ol>
+									</td>
+								</tr>
+								<tr>
+									<th>기타</th>
+									<td colspan="2">
+										<ol class="circle-list">
+											<dl class="dl-inline">
+												<dt>기타 대학의 전형료 반환 기준표</dt>
+												<dd>증빙서류 제출 시</dd>
+											</dl>
+											<li>
+												<dl class="dl-inline">
+													<dt>원서접수 마감 전</dt>
+													<dd>원서접수 수수료(5,000원)를 제외한 나머지 금액</dd>
+												</dl>
+											</li>
+											<li>
+												<dl class="dl-inline">
+													<dt>원서접수 마감 후 실기고사 기간</dt>
+													<dd>원서접수 수수료(5,000원)를 제외한 응시료(20,000원)만 반환</dd>
+												</dl>
+											</li>
+											<li>
+												<dl class="dl-inline">
+													<dt>실기고사 종료 후</dt>
+													<dd>반환하지 않음</dd>
+												</dl>
+											</li>
+										</ol>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</section>
+		</div>
+	</main>
+<style>
+/* submission */
+.recruitment.submission .submission-document .table-wrap .table {margin-bottom:20px; }
+.recruitment.submission .submission-document .table-wrap .table tbody tr td:first-child {
+	text-align:center;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list {
+	text-align:left;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list li {
+	line-height:20px;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list li a {
+	color:#666;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list li a:hover {
+	text-decoration:underline;
+	color:#666;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list .after-list {
+	position:relative !important;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .circle-list .after-list:after {
+	position:absolute;
+	top:0;
+	left:205px;
+	content:":";
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td .attention:before {
+	padding-right:3px;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td h4:first-child {
+	margin-top:0;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td h4 {
+	margin:0;
+	font-size:14px; 
+	color:#666;
+	font-weight:300;
+	margin-top:30px;
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td h4:before {
+	content:"[";
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr td h4:after {
+	content:"]";
+}
+.recruitment.submission .submission-document .table-wrap .table tbody tr:last-child {
+	height:586px;
+}
+.recruitment.submission .submission-document .table-wrap .attention {
+	line-height:24px;
+}
+.recruitment.submission .submission-document .table-wrap .attention b {
+	color:#333;
+	font-weight:400;
+}
+
+
+
+</style>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>

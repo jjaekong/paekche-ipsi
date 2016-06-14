d3781fb2e10217d6a42e0c7146a835741b18fa95
kyh5392 <kyh90495392@gmail.com>
2016-06-14 14:42:36 +0900

    [모집요강] > 전형일정 (수시1차, 2차, 정시) , 모집학과 및 모집인원 (수시1차, 2차, 정시) #1 완료.

---
+++ recruitment/schedule/recruit_schedule.php
@@ -0,0 +1,254 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+<link rel="stylesheet" href="/assets/css/schedule.css" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="jumbo-wall">
+		<div class="container">
+			<p>
+				<small>우리문화예술의 정통성을 바탕으로 그 본류를 주도하고</small><br>
+				미래를 선도하는 예술인 교육
+			</p>
+		</div>
+	</div>
+	<div class="page-nav">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">입시정보</a></li>
+				<!-- li class="active">수업자료실</li -->
+				<li class="active dropdown">
+					<a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">전형일정<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">제출서류 및 전형료</a></li>
+					</ul>
+				</li>
+			</ol>
+		</div>
+	</div>
+	<main id="content" class="schedule recruit-schedule">
+		<div class="container">
+			<section class="schedule">
+				<div class="section-header">
+					<hr />
+					<h3>전형일정</h3>
+				</div>
+				<div class="section-content">
+					<ul class="nav nav-pills nav-justified" role="tablist">
+						<li role="presentation" class="active"><a href="#tab-1" aria-controls="tab-1" role="tab" data-toggle="tab">수시 1차 모집</a></li>
+						<li role="presentation"><a href="#tab-2" aria-controls="tab-2" role="tab" data-toggle="tab">수시 2차 모집</a></li>
+						<li role="presentation"><a href="#tab-3" aria-controls="tab-3" role="tab" data-toggle="tab">정시 모집</a></li>
+					</ul>
+					<div class="tab-content">
+						<!-- 수시1차 -->
+						<div role="tabpanel" class="tab-pane active tab-1" id="tab-1">
+							<div class="table-wrap">
+								<table class="table table-border">
+									<colgroup>
+										<col style="width: 238px"/>
+										<col style="width: 600px"/>
+										<col style="width: 332px"/>
+									</colgroup>
+									<thead> 
+										<tr>
+											<th>구분</th>
+											<th>수시1차 모집일정</th>
+											<th>비고</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>원서접수</td>
+											<td>2015.9.2(수).09:00 ~ 9.24(목). 17:00</td>
+											<td>유웨이어플라이, 진학어플라이</td>
+										</tr>
+										<tr>
+											<td>서류제출</td>
+											<td>2015.9.2(수).09:00 ~ 9.24(목). 17:00</td>
+											<td>마감일 우체국 소인까지 유효</td>
+										</tr>
+										<tr>
+											<td>실기고사 공지</td>
+											<td>2015.9.25(금). 11:00</td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>실기고사</td>
+											<td>2015.10.12(월) ~ 10.16(금)</td>
+											<td>장소 : 서울 삼성동 아트센터</td>
+										</tr>
+										<tr>
+											<td>합격자 발표</td>
+											<td>2015.10.21(수). 14:00</td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>예치금 등록</td>
+											<td>2015.12.11(금) ~ 12.14(월). 16:00</td>
+											<td>개별 가상계좌</td>
+										</tr>
+										<tr>
+											<td>미등록 충원</td>
+											<td>2015.12.15(화) ~ 12.23(수).16:00</td>
+											<td>-</td>
+										</tr>
+										<tr>
+											<td>최종등록</td>
+											<td>2016.1.29(금) ~ 2.2(화). 16:00</td>
+											<td>개별 가상계좌</td>
+										</tr>
+										<tr>
+											<td>입학문의</td>
+											<td colspan="2"><p class="tel"><a href="tel:0632609001">(063)260-9001</a> [전북 완주군 봉동읍 백제대학로 171 홍보처]</p></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<div role="tabpanel" class="tab-pane tab-2" id="tab-2">
+							<div class="table-wrap">
+								<table class="table table-border">
+									<colgroup>
+										<col style="width: 238px"/>
+										<col style="width: 600px"/>
+										<col style="width: 332px"/>
+									</colgroup>
+									<thead> 
+										<tr>
+											<th>구분</th>
+											<th>수시2차 모집일정</th>
+											<th>비고</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>원서접수</td>
+											<td>2015.11.3(화).09:00 ~ 11.17(화).17:00</td>
+											<td>유웨이어플라이, 진학어플라이</td>
+										</tr>
+										<tr>
+											<td>서류제출</td>
+											<td>2015.11.3(화).09:00 ~ 11.17(화).17:00</td>
+											<td>마감일 우체국 소인까지 유효</td>
+										</tr>
+										<tr>
+											<td>실기고사 공지</td>
+											<td>2015.11.18(수). 11:00</td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>실기고사</td>
+											<td>2015.11.23(월) ~ 11.27(금)</td>
+											<td>장소 : 서울 삼성동 아트센터</td>
+										</tr>
+										<tr>
+											<td>합격자 발표</td>
+											<td>2015.12.2(수). 14:00</td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>예치금 등록</td>
+											<td>2015.12.11(금) ~ 12.14(월). 16:00</td>
+											<td>개별 가상계좌</td>
+										</tr>
+										<tr>
+											<td>미등록 충원</td>
+											<td>2015.12.15(화) ~ 12.23(수). 16:00</td>
+											<td>-</td>
+										</tr>
+										<tr>
+											<td>최종등록</td>
+											<td>2016.1.29(금) ~ 2.2(화). 16:00</td>
+											<td>개별 가상계좌</td>
+										</tr>
+										<tr>
+											<td>입학문의</td>
+											<td colspan="2"><p class="tel"><a href="tel:0632609001">(063)260-9001</a> [전북 완주군 봉동읍 백제대학로 171 홍보처]</p></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+						<div role="tabpanel" class="tab-pane tab-3" id="tab-3">
+							<div class="table-wrap">
+								<table class="table table-border">
+									<colgroup>
+										<col style="width: 238px"/>
+										<col style="width: 600px"/>
+										<col style="width: 332px"/>
+									</colgroup>
+									<thead> 
+										<tr>
+											<th>구분</th>
+											<th>정시모집 일정</th>
+											<th>비고</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>원서접수</td>
+											<td>2015.12.24(목).09:00 ~ 2016.1.5(화). 17:00</td>
+											<td>유웨이어플라이, 진학어플라이</td>
+										</tr>
+										<tr>
+											<td>서류제출</td>
+											<td>2015.12.24(목).09:00 ~ 2016.1.5(화). 17:00</td>
+											<td>마감일 우체국 소인까지 유효</td>
+										</tr>
+										<tr>
+											<td>실기고사 공지</td>
+											<td>2016.1.6(수). 11:00</td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>실기고사</td>
+											<td>2016.1.11(월) ~ 1.15(금)</td>
+											<td>장소 : 서울 삼성동 아트센터</td>
+										</tr>
+										<tr>
+											<td>합격자 발표</td>
+											<td>2016.1.20(수). 14:00 </td>
+											<td>홈페이지 (입학안내) 공지</td>
+										</tr>
+										<tr>
+											<td>등록기간</td>
+											<td>2016.1.29(금) ~ 2.2(화). 16:00</td>
+											<td>지로 또는 개별 가상계좌</td>
+										</tr>
+										<tr>
+											<td>추가모집</td>
+											<td>2016.2.3(수) ~ 2.27(토). 18:00</td>
+											<td></td>
+										</tr>
+										<tr>
+											<td>입학문의</td>
+											<td colspan="2"><p class="tel"><a href="tel:0632609001">(063)260-9001</a> [전북 완주군 봉동읍 백제대학로 171 홍보처]</p></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="point">
+				<div class="section-header">
+					<hr />
+					<h3>지원 시 유의사항</h3>
+				</div>
+				<div class="section-content">
+					<ul class="content-list">
+						<li>전문대학은 수시모집 시 <b>지원 횟수(6회) 제한이 없습니다. </b></li>
+						<li>수시모집에서 충원 합격자는 수험생의 등록의사와 관계없이 합격으로 처리되며, 수시 최초의 합격자나 충원 합격자는 정시모집 또는 추가모집에 지원이 불가합니다. </li>
+						<li>수시모집 등록금을 2개 이상의 대학에 납부한 경우(등록예치금 납부도 정식등록으로 처리), 이중등록금지 위반으로 모든 대학의 입학이 취소됩니다.</li>
+						<li>학년도와 학기가 동일한 2개 이상의 대학에 합격한 자는 최종적으로 하나의 대학에만 등록해야 하며, 이를 위반한 경우에는 모든 대학의 입학이 취소됩니다.</li>
+						<li>최종 등록 마감 후, 모든 대학 지원자의 지원/합격/등록 자료를 전산 검색하여 "대입지원 위반자"로 검출되는 경우에는 대학 입학이 취소됩니다. </li>
+					</ul>
+				</div>
+			</section>
+		</div>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>

1db73b5393e5053c32abecbd2525425f08c6d5dd
kyh5392 <kyh90495392@gmail.com>
2016-06-15 14:13:34 +0900

    [입시정보] > 백제스타 #1 완료.

---
+++ information/star_list.php
@@ -0,0 +1,342 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead_sub.php'); ?>
+<link rel="stylesheet" href="/assets/css/information.css" />
+</head>
+<body>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+	<div class="jumbo-wall">
+		<div class="container">
+			<p>
+				<small>우리문화예술의 정통성을 바탕으로 그 본류를 주도하고</small><br>
+				미래를 선도하는 예술인 교육
+			</p>
+		</div>
+	</div>
+	<div class="page-nav">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+				<li><a href="#">입시정보</a></li>
+				<!-- li class="active">수업자료실</li -->
+				<li class="active dropdown">
+					<a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">백제스타<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">경쟁률</a></li>
+					</ul>
+				</li>
+			</ol>
+		</div>
+	</div>
+	<main id="content" class="information star">
+		<section class="container">
+			<div class="section-header">
+				<hr />
+				<h3>백제스타</h3>
+			</div>
+			<div class="section-content">
+				<div class="search-area">
+					<form>
+						<div class="form-group">
+							<label for="search-keyword" class="sr-only">검색어</label>
+							<input type="text" name="#" id="search-keyword" class="form-control" placeholder="Search">
+						</div>
+						<button type="submit" class="btn"><span class="glyphicon glyphicon-search"></span><span class="sr-only">검색</span></button>
+					</form>
+				</div>
+				<div class="star-wrap">
+					<div class="star-view" id="star-view">
+						<ul>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+							<li>
+								<figure>
+									<img src="/assets/images/information/img_star00.jpg" alt="">
+									<figcaption>
+										<dl>
+											<dt>윤손하</dt>
+											<dd>탤런트</dd>
+											<dd class="br" role="separator"></dd>
+											<dd>방송연예</dd>
+											<dd>94학번</dd>
+										</dl>
+									</figcaption>
+								</figure>
+							</li>
+						</ul>
+						<div class="controls">
+							<a class="prev" href="#"><img src="/assets/images/information/ico_arrow_left.png" alt="이전"></a>
+							<a class="next" href="#"><img src="/assets/images/information/ico_arrow_right.png" alt="다음"></a>
+						</div>
+					</div>
+					<div class="star-list">
+						<ul>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star01.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>백지영</dt>
+												<dd>가수</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star02.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>유리</dt>
+												<dd>가수</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star03.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>원빈</dt>
+												<dd>영화배우</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star04.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>김병만</dt>
+												<dd>개그맨</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star05.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>안영미</dt>
+												<dd>개그우먼</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star06.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>신동</dt>
+												<dd>가수</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star07.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>장우혁</dt>
+												<dd>가수</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+							<li>
+								<a href="#star-view">
+									<figure>
+										<img src="/assets/images/information/img_star08.jpg" alt="">
+										<figcaption>
+											<dl>
+												<dt>이수</dt>
+												<dd>가수</dd>
+											</dl>
+										</figcaption>
+									</figure>
+								</a>
+							</li>
+						</ul>
+					</div>
+					<nav class="paging">
+						<nav class="paging">
+							<ul class="pagination">
+								<li>
+									<a href="#" aria-label="first-Previous">
+										<span aria-hidden="true">&laquo;</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" aria-label="Previous">
+										<span aria-hidden="true">&lsaquo;</span>
+									</a>
+								</li>
+								<li class="active"><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+								<li>
+									<a href="#" aria-label="Next" >
+										<span aria-hidden="true">&rsaquo;</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" aria-label="end-Next">
+										<span aria-hidden="true">&raquo;</span>
+									</a>
+								</li>
+							</ul>
+						</nav>
+						<div class="btn-area">
+							<p>
+								<a href="#" class="btn btn-pink">글쓰기</a>
+							</p>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</section>
+	</main>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+	<script src="/assets/js/jquery.bxslider.min.js"></script>
+	<script>
+		(function($) {
+			var starSlider = $('.star-view > ul').bxSlider({
+				controls: false,
+				pager: false 
+			});
+			
+			$(document).on('click', '.star-list > ul > li > a', function(e) {
+				var clickedIndex = $(this).parent().index();
+				starSlider.goToSlide(clickedIndex);
+			});
+			
+			$(document).on('click', '.star-view > .controls > .prev', function(e) {
+				starSlider.goToPrevSlide();
+				e.preventDefault();
+			});
+			
+			$(document).on('click', '.star-view > .controls > .next', function(e) {
+				starSlider.goToNextSlide();
+				e.preventDefault();
+			});
+			
+		})(jQuery);
+	</script>
+</body>
+</html>
